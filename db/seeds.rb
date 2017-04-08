2d8f130c2f5f2075b4ebaadfa5cb3b3e8edeb149
Mike Bonetti <mbonettimedia@gmail.com>
2017-04-08 17:15:55 -0500

    add seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10 * User.create!(
+  username: Faker::Name.name
+  email: Faker::Internet.email
+  password: 'password'
+  )
+
+
+10 * Post.create!(
+  author_id: [1,2,3,4,5,6,7,8,9,10].sample
+  title: Faker::Book.title
+  text: Faker::Lorem.paragraph
+  published?: Faker::Boolean.boolean
+  )
+
+10 * Vote.create!(
+  voter_id: [1,2,3,4,5,6,7,8,9,10].sample
+  votable_id: [1,2,3,4,5,6,7,8,9,10].sample
+  votable_type: ['comment', 'post'].sample
+  )
