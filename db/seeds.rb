d231693c3e0486f715c897770004a037ee3e93ba
Mike Bonetti <mbonettimedia@gmail.com>
2017-04-08 17:30:06 -0500

    Minor changes to migrations, seeds now work

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,22 +6,25 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-10 * User.create!(
-  username: Faker::Name.name
-  email: Faker::Internet.email
+10.times do  User.create!(
+  username: Faker::Name.name,
+  email: Faker::Internet.email,
   password: 'password'
   )
+end
 
 
-10 * Post.create!(
-  author_id: [1,2,3,4,5,6,7,8,9,10].sample
-  title: Faker::Book.title
-  text: Faker::Lorem.paragraph
+10.times do  Post.create!(
+  author_id: [1,2,3,4,5,6,7,8,9,10].sample,
+  title: Faker::Book.title,
+  text: Faker::Lorem.paragraph,
   published?: Faker::Boolean.boolean
   )
+end
 
-10 * Vote.create!(
-  voter_id: [1,2,3,4,5,6,7,8,9,10].sample
-  votable_id: [1,2,3,4,5,6,7,8,9,10].sample
-  votable_type: ['comment', 'post'].sample
+10.times do Vote.create!(
+  voter_id: [1,2,3,4,5,6,7,8,9,10].sample,
+  votable_id: [1,2,3,4,5,6,7,8,9,10].sample,
+  votable_type: ['Comment', 'Post'].sample
   )
+end
