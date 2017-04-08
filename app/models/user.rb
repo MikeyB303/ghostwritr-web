73d57fb58bbbe3933f3f9e5ff1384323db17bd34
Ken Saganski <saganawski@gmail.com>
2017-04-08 15:07:47 -0500

    add user associations and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+	has_secure_password
+	has_many :posts, foreign_key: :author_id
+	has_many :votes, foreign_key: :voter_id
+	has_many :comments, foreign_key: :commenter_id
+
+	validates :username, :email, :password_digest, presence: true
+    validates :email, uniqueness: true
 end
