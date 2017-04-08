4d3c9284fea11b9b9c55c5b47340a09493cc119e
Ken Saganski <saganawski@gmail.com>
2017-04-08 17:05:42 -0500

    restrict user routes to only new and create

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'home#index'
   get '/shakespeare' => 'home#shakespeare'
   get '/rowling' => 'home#rowling'
-  # TODO restrict user paths
-  resources :users 
+
+  resources :users, only: [:new, :create]
 
 end
