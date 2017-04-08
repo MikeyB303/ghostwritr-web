ff432f1681ac25313e464b46b5b31f7ea23e4097
Mike Bonetti <mbonettimedia@gmail.com>
2017-04-08 18:21:34 -0500

    route changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'home#index'
+  root 'posts#index'
   get '/shakespeare' => 'home#shakespeare'
   get '/rowling' => 'home#rowling'
+
+  resources :posts
 end
