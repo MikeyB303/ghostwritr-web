477f8fd33de6da6d9439ae7c5aca466a62d864ba
Ken Saganski <saganawski@gmail.com>
2017-04-08 21:56:16 -0500

    pull post to its own resources, add a nested comments routes under post

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,9 @@ Rails.application.routes.draw do
   get '/shakespeare' => 'home#shakespeare'
   get '/rowling' => 'home#rowling'
 
-  resources :users, only: [:new, :create] do
-    resources :posts
+  resources :posts do 
   	resources :comments
   end
+  resources :users, only: [:new, :create] 
   resources :sessions, only: [:new, :create, :destroy]
 end
