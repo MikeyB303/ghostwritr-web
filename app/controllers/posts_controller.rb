517ca894167acad1abe2ab5fbcbb05f93486bff1
Mike Bonetti <mbonettimedia@gmail.com>
2017-04-08 19:14:13 -0500

    Add link to create new post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,4 +4,12 @@ class PostsController < ApplicationController
     @posts = Post.all.sort_by{|post| post.votes.count}.reverse 
   end
 
+  def new
+  end
+
+  private
+  def post_params
+    params.require(:posts).permit(:author_id, :title, :text)
+  end
+
 end
