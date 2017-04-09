a7a455987c0a2d14645f0c7d7d36d6b64658aa0c
Mike Bonetti <mbonettimedia@gmail.com>
2017-04-09 12:13:16 -0500

    Add link to author show page on nav bar if logged in

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
   resources :posts do 
   	resources :comments, only: [:new, :create, :destroy]
   end
-  resources :users, only: [:new, :create] 
+  resources :users, only: [:new, :create, :show] 
   resources :sessions, only: [:new, :create, :destroy]
 end
