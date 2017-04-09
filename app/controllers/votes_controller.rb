a4e359630a111e89f8ce51dcfbcf222d5ee14aec
Ken Saganski <saganawski@gmail.com>
2017-04-09 14:59:07 -0500

    user can up vote comment

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,45 +1,24 @@
 class VotesController < ApplicationController
-	before_action :load_voteable
 
 	
-	# def create
-	# 	puts "*" * 30
-	# 	if params[:comment_id]
-	# 		comment = Comment.find_by(id:params[:comment_id])
-	# 		vote = comment.votes.new(voter_id: current_user)
-	# 		if comment.save
-	# 			redirect_to post_path(params[:post_id])
-	# 		else
-	# 			"sucks to suck"
-	# 		end
-	# 	else
-	# 		post = Post.find_by(id: params[:post_id])
-	# 		vote = post.votes.new(voter_id: current_user.id)
-	# 		if vote.save
-	# 			redirect_to post_path(params[:post_id])
-	# 		else
-	# 			"sucks to suck"
-	# 		end
-	# 	end
-	# end
-	def new
-		@vote = @voteable.votes.new
-	end
-
-	def create 
-		@vote = @voteable.votes.new(allowed_params)
-		if @vote.save
-			redirect_to post_path(params[:post_id])
+	def create
+		if params[:comment_id]
+			comment = Comment.find_by(id:params[:comment_id])
+			vote = comment.votes.create!(voter_id: current_user.id)
+			if comment.save
+				redirect_to post_path(comment.post.id)
+			else
+				"sucks to suck"
+			end
 		else
+			post = Post.find_by(id: params[:post_id])
+			vote = post.votes.new(voter_id: current_user.id)
+			if vote.save
+				redirect_to post_path(params[:post_id])
+			else
+				"sucks to suck"
+			end
 		end
-
-	end
-
-	private
-
-	def load_voteable
-		resource, id = request.path.split('/')[1,2]
-		@voteable = resource.singularize.classify.constantize.find(id)
 	end
 
 end
