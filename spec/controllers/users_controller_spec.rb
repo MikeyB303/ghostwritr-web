a9e3cc7b75c829ba988c22ec0ce37d181abd9c5b
Mike Bonetti <mbonettimedia@gmail.com>
2017-04-09 20:46:21 -0500

    Add some post controller tests

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -30,6 +30,7 @@ describe UsersController do
     let!(:user) {User.create!(username: "mikey", email: "test@email.com", password: "password")}
     let!(:published_post) {Post.create!(author_id: user.id, title: "Harry Potter 20", text: "Wacky Wizards", published?: true)}
     let!(:unpublished_post) {Post.create!(author_id: user.id, title: "Harry Potter 20", text: "Wacky Wizards", published?: false)}
+    
     it 'renders users#show' do
       get :show, {params: {id: user.id}}
       expect(response).to render_template'show'
