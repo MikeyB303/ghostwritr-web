61d180227ffc9ef6dcb0efb5c79cbad46d07a566
Mike Bonetti <mbonettimedia@gmail.com>
2017-04-09 19:55:28 -0500

    Add user controller tests

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -25,4 +25,30 @@ describe UsersController do
       expect{post :create, {params: {users: {username: "", email: "test@email.com", password: "password"}}}}.to change{User.all.length}.by(0)
     end
   end
+
+  describe 'User show page' do
+    let!(:user) {User.create!(username: "mikey", email: "test@email.com", password: "password")}
+    let!(:published_post) {Post.create!(author_id: user.id, title: "Harry Potter 20", text: "Wacky Wizards", published?: true)}
+    let!(:unpublished_post) {Post.create!(author_id: user.id, title: "Harry Potter 20", text: "Wacky Wizards", published?: false)}
+    it 'renders users#show' do
+      get :show, {params: {id: user.id}}
+      expect(response).to render_template'show'
+    end
+
+    it 'assigns @user to the page owner' do
+      get :show, {params: {id: user.id}} 
+      expect(assigns(:user)).to eq(User.find_by id: user.id)
+    end
+
+    it 'assigns @user_published_posts to published posts' do
+      get :show, {params: {id: user.id}} 
+      expect(assigns(:user).posts.select {|post| post.published?}).to include(published_post) 
+    end
+
+    it 'assigns @user_unpublished_posts to unpublished posts' do
+      get :show, {params: {id: user.id}} 
+      expect(assigns(:user).posts.reject {|post| post.published?}).to include(unpublished_post)
+    end
+
+  end
 end
