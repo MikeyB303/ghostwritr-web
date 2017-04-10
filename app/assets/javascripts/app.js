38935093af1cc16a8c60552fb454587a114267ec
Armando Dollia <dollia.armando@gmail.com>
2017-04-09 20:47:03 -0500

    fixes turbolinks

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,14 +1,5 @@
-$(document).ready(function () {
+$(document).on('turbolinks:load', function () {
   var apiDestination;
-
-
-  // $('app-container').replaceWith(
-  //     "<textarea disabled='true' id='user_text'/>" +
-  //     "<br>" + "<ul id='predicted_text'></ul>" +
-  //     "<button class='inspirational_author' data-author='/shakespeare'>Shakespeare</button>" + "<br>" +
-  //     "<button class='inspirational_author' data-author='/rowling'>J-K Rowling</button>" + "<br>" +
-  //     "<button class='inspirational_author' data-author='/got'>George RR Martin</button>"
-  // );
 
   $('#libraries').on('click', '.inspirational_author', function (event) {
     var $library = $(this);
@@ -16,9 +7,10 @@ $(document).ready(function () {
     $('#user-text').val('');
     apiDestination = $library.data('author');
     $('#user-text').prop('disabled', false);
+  //  TODO fix text area not enabling without hard reset
   });
 
-  $('#post-form').on('keyup', '#user-text', function (event) {
+  $('#post-form').on('keydown', '#user-text', function (event) {
     if(event.which === 32){
     console.log(apiDestination);
       var userText = $(this).serialize();
@@ -34,19 +26,6 @@ $(document).ready(function () {
     }
   });
 
-  // function formatPredictions(predictions) {
-  //   var probableWords = [];
-  //   $.each(predictions, function (word) {
-  //     if (probability > 0.01){
-  //       probableWords.push({word: word, probability: probability});
-  //     }
-  //   });
-  //   probableWords.sort(function (a, b) {
-  //     return b.probability - a.probability
-  //   });
-  //
-  //   return probableWords
-  // }
 
   function renderWords(wordsArray) {
     var $predictionField = $('#predicted_text');
