9bbba46f9939bd18940ea7d7ce7cc2d6985887a7
Armando Dollia <dollia.armando@gmail.com>
2017-04-11 15:44:02 -0500

    Implements selecting predictions with keyboard

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -3,12 +3,15 @@ $(document).on('turbolinks:load', function () {
    var $postForm =  $('#post-form');
 
   $('#libraries').change(function () {
-    $('#predictions').empty();
+    $('#predictions').empty().addClass('hide');
     apiDestination = $(this).find(':selected').data('author');
     $('#user-text').prop('disabled', false);
   });
 
   $postForm.on('keydown', '#user-text', function (event) {
+    var $predictions = $('#predictions');
+    var $active;
+
     if(event.which === 32){
       var userText = $(this).serialize();
       var response = $.ajax({
@@ -18,17 +21,42 @@ $(document).on('turbolinks:load', function () {
 
       response.done(function (predictions) {
         renderWords(predictions);
+        $predictions.find('a').first().addClass('active')
       });
     }
-  });
 
-  $postForm.on('keydown', '#user-text', function (event) {
-    var $predictions = $('#predictions');
-    var $active;
-    if(event.which === 40){
-      if($predictions.find('a').hasClass('active')){
-        $active = $('#predictions .active');
+    else if(event.which === 40){
+      event.preventDefault();
+      $active = $predictions.find('.active');
+      if($active.next().is('.collection-item')){
+        $active.removeClass('active');
+        $active = $active.next();
+        $active.addClass('active')
+      }else{
+       $active.removeClass('active');
+       $predictions.find('a').first().addClass('active')
       }
+    }
+
+    else if(event.which === 38){
+      event.preventDefault();
+      $active = $predictions.find('.active');
+      if($active.prev().is('.collection-item')){
+        $active.removeClass('active');
+        $active = $active.prev();
+        $active.addClass('active')
+      }
+      else{
+        $active.removeClass('active');
+        $predictions.find('a').last().addClass('active')
+      }
+    }
+
+    else if(event.which === 13){
+      event.preventDefault();
+      var $userText = $('#user-text');
+      $active = $predictions.find('.active');
+      $userText.val($userText.val() + $active.text())
     }
   });
 
