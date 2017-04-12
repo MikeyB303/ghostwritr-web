d4d6eeeee3ec35ff3ce6c7e084f81517037e1e7d
Ken Saganski <saganawski@gmail.com>
2017-04-12 17:19:37 -0500

    add helper method to find if user has any published post

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -2,4 +2,11 @@ module PostsHelper
   def published_posts(user, published = true)
     user.posts.where(published?: published)
   end
+
+  def return_true_if_user_has_published_post(user)
+  	user.posts.each do |post|
+  		return true if post.published? == true
+  	end
+  	return false
+  end
 end
